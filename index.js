27011946672b2e5cf9ec1c720593131efbf81d61
rr6361192-spec <rr6361192@gmail.com>
2026-05-01 23:50:05 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,21 +1,68 @@
-import express from 'express';
+import dotenv from "dotenv";
+dotenv.config();
+import cookieParser from "cookie-parser";
+import express from "express";
+
+const PORT = process.env.PORT || 4000;
 
 const app = express();
-const PORT = 3000;
+app.use(cookieParser())
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+app.set("view engine", "ejs");
+
+const Session = {};
+
+function gen() {
+  return Math.random().toString(36).substring(2, 10);
+}
+
+function create(id) {
+  if (!Session[id]) {
+    Session[id] = {};
+  }
+  return Session[id];
+}
 
 
+app.get("/", (req, res) => {
+  res.render("login");
+});
 
 
-app.get('/', (req, res) => {
-  res.send('<h1> Backend Project - Day 1!</h1>');
+app.post("/login", (req, res) => {
+  const { name, email, sessionId } = req.body;
+
+  if (!name || !email) {
+    return res.status(400).json({ error: "Name and email required" });
+  }
+
+  const id = sessionId || gen();
+  const session = create(id);
+
+  session.name = name;
+  session.email = email;
+res.cookie("sessionId", id);
+
+ 
+  res.json({ sessionId: id, session });
+})
+
+app.get("/dash", (req, res) => {
+  const sessionId = req.cookies.sessionId;
+  
+  const session = Session[sessionId];
+
+  if (!session) {
+    return res.status(401).send("Unauthorized");
+  }
+
+  res.send(`Welcome ${session.name}`);
 });
 
 
 
-
-
-
-// Start server
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
---
+++ kk.js
@@ -0,0 +1,11 @@
+//javascript
+
+(function solve(input){
+    if(input){
+        console.log("rama mam hii")
+    }
+    else{
+        console.log("mam not hii")
+    }
+})();
+
